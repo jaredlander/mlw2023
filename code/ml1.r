cd981567c97680bdbf12cfbd139b791684971820
Jared Lander <info@jaredlander.com>
2023-06-18 21:28:27 +0000

    Showed tuing

--- code/ml1.r
+++ code/ml1.r
@@ -160,3 +160,125 @@ library(yardstick)
 loss_fn <- metric_set(roc_auc, mn_log_loss, accuracy)
 loss_fn <- metric_set(roc_auc)
 loss_fn
+
+# Cross-Validation ####
+
+# https://twitter.com/chrisalbon/status/1664318913470169088
+
+set.seed(43)
+theCV <- vfold_cv(
+    data=train,
+    v=5,
+    repeats=1,
+    strata='Status'
+)
+theCV
+theCV$splits[[1]]
+data_split
+theCV$splits[[1]] |> assessment()
+theCV$splits[[1]] |> testing()
+testing
+
+
+library(tune)
+
+val1 <- fit_resamples(
+    flow1,
+    resamples=theCV,
+    metrics=loss_fn
+)
+
+val2 <- fit_resamples(
+    flow2,
+    resamples=theCV,
+    metrics=loss_fn
+)
+
+val1
+val1$.metrics[[1]]
+val1$.metrics[[2]]
+
+val1 |> collect_metrics()
+val2 |> collect_metrics()
+
+# Tuning ####
+
+spec3 <- boost_tree(
+    mode='classification',
+    trees=tune(),
+    tree_depth=tune(),
+    sample_size=0.8
+)
+spec2
+spec3
+
+flow3 <- flow2 |> 
+    update_model(spec3)
+flow3
+
+library(dials)
+
+flow3 |> extract_parameter_set_dials()
+flow3 |> extract_parameter_set_dials() |> 
+    pull(object)
+
+params3 <- flow3 |> 
+    extract_parameter_set_dials() |> 
+    update(
+        trees=trees(c(25, 500)),
+        tree_depth=tree_depth(c(2, 6))
+    )
+params3 |> pull(object)
+
+
+set.seed(24)
+grid3 <- grid_latin_hypercube(params3, size=100)
+grid3
+
+grid3 |> 
+    group_nest(tree_depth) |> 
+    mutate(data=purrr::map(data, \(x) arrange(x, trees))) |> 
+    pull(data)
+
+library(tictoc)
+
+library(parallel)
+library(doFuture)
+
+registerDoFuture()
+cl <- makeCluster(5)
+plan(cluster, workers=cl)
+
+options(tidymodels.dark=TRUE)
+tic(msg='tuning 3')
+tuned3 <- tune_grid(
+    flow3,
+    resamples=theCV,
+    grid=grid3,
+    metrics=loss_fn,
+    control=control_grid(
+        verbose=TRUE,
+        allow_par=TRUE
+    )
+)
+toc(log=TRUE)
+
+tuned3
+tuned3$.metrics[[1]]
+
+tuned3 |> collect_metrics()
+
+tuned3 |> autoplot()
+
+tuned3 |> show_best(n=10)
+
+tuned3 |> select_best()
+tuned3 |> select_by_one_std_err(trees, tree_depth)
+best_params3 <- tuned3 |> 
+    select_by_one_std_err(trees, tree_depth)
+
+mod3 <- flow3 |> finalize_workflow(parameters=best_params3)
+mod3
+
+fit3 <- mod3 |> fit(data=train)
+fit3
